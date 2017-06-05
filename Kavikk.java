9c90faab2dcb648a99d759d840695857b2929fbc
kavipriya2812 <kavipriyakailasam@gmail.com>
2017-06-05 15:51:35 +0530

    Add files via upload

---
+++ Kavikk.java
@@ -0,0 +1,28 @@
+package kavikk;
+import java.util.Scanner;
+
+public class Kavikk {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n,f;
+		Scanner scan=new Scanner(System.in);
+		System.out.println("enter a number");
+		f=scan.nextInt();
+		if(f==0)
+		{
+			System.out.println("zero");
+		}
+		if(f>0)
+		{
+			System.out.println("positive");
+		}
+		if(f<0)
+		{
+			System.out.println("negative");
+		}
+
+			}
+
+		}
+
