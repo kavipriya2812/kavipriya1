b129cc20bb262f8146f72a4c6357808f0c527f98
kavipriya2812 <kavipriyakailasam@gmail.com>
2017-06-06 14:55:34 +0530

    Add files via upload

---
+++ Helo.java
@@ -0,0 +1,11 @@
+package helo;
+
+public class Helo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String str="hello";
+System.out.println(str+str+str+str+str);
+	}
+
+}
