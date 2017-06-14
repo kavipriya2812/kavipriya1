a282278b5ec2d2025018bb24ad330fd022546c39
kavipriya2812 <kavipriyakailasam@gmail.com>
2017-06-14 21:47:25 +0530

    Add files via upload

---
+++ Reverseastring.java
@@ -0,0 +1,10 @@
+package player;
+public class Reverseastring {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+StringBuffer s=new StringBuffer("guvi");
+s.reverse();
+System.out.println("the reverse string is"+s);
+	}
+}
