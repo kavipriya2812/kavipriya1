efe66b5773ef0a75d378960945b7a67b3edf6c6f
kavipriya2812 <kavipriyakailasam@gmail.com>
2017-06-15 09:07:45 +0530

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,16 @@
+package beginner;
+
+public class factorial {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int n=3;
+		int i;
+		int factorial=1;
+for(i=1;i<=n;i++)
+{
+	factorial=factorial*i;
+}
+System.out.println("the factorial is"+factorial);
+	}
+
+}
