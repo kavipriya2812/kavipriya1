9382ceab60b5c962169803dfadf9e6b6b863f919
kavipriya2812 <kavipriyakailasam@gmail.com>
2017-06-15 09:20:13 +0530

    Add files via upload

---
+++ Stringtointeger.java
@@ -0,0 +1,15 @@
+package beginner;
+
+public class Stringtointeger {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String number="2";
+int g=Integer.parseInt(number);
+System.out.println("the integer is"+g);
+	}
+
+}
