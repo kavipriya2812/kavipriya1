0b0a585e9119074e6d8493f4ff47e69e8c0226e5
kavipriya2812 <kavipriyakailasam@gmail.com>
2017-07-28 14:33:29 +0530

    Add files via upload

---
+++ mmmmmmm.java
@@ -0,0 +1,13 @@
+package rrr;
+import java.util.*;
+public class mmmmmmm {
+
+	public static void main(String[] args) {
+		 Scanner scan=new Scanner(System.in);
+		 String str=scan.nextLine();
+		   String[] words=str.split(" ");
+		    for(int j=words.length-1;j>=0;j--){
+		      System.out.print(words[j]+" ");
+	}
+
+}}
