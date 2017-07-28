f964b83179edbc25482e9447c019e5313c4955f7
kavipriya2812 <kavipriyakailasam@gmail.com>
2017-07-28 14:31:21 +0530

    Add files via upload

--- jkhjh.java
+++ jkhjh.java
@@ -3,17 +3,22 @@ import java.util.Scanner;
 public class jkhjh {
 
 	public static void main(String[] args) {
+		int count=0;
 		Scanner scan=new Scanner(System.in);
 		String a=scan.nextLine();
 		String b=scan.nextLine();
 		int l=a.length();
-		int  v=b.length();
+		int v=b.length();
 		for(int i=0;i<=l-v;i++){
 			if(a.substring(i,v+i).equals(b)){
 				System.out.println(i);
 			}
 			else{
-				System.out.println("-1");break;}}
+				count++;
+				}
+			}
+		if(count==(l-v)+1){
+			System.out.println("-1");
 			}
 
-}
+}}
