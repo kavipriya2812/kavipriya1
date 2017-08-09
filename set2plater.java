5985cb3bf77e619e44642fd6edfd88f3a1d4df60
kavipriya2812 <kavipriyakailasam@gmail.com>
2017-08-09 10:38:20 +0530

    Add files via upload

---
+++ set2plater.java
@@ -0,0 +1,16 @@
+package rrr;
+import java.util.*;
+public class set2plater {
+
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		String day=scan.nextLine();
+		if(day.equals("saturday") || day.equals("sunday")){
+			System.out.println("false");
+		}else{
+			System.out.println("true");
+		}
+
+	}
+
+}
