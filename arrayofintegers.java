fe75fefb24026ac3f7904d6b7d406272e54235c9
kavipriya2812 <kavipriyakailasam@gmail.com>
2017-08-09 15:32:44 +0530

    Add files via upload

---
+++ arrayofintegers.java
@@ -0,0 +1,30 @@
+package rrr;
+import java.util.*;
+public class arrayofintegers {
+
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		int a[]={1,1,2,2,3,3,4,4,5,5,7};
+		int count=0;
+		for(int i=0;i<11;i++){
+			boolean flag=true;count=0;
+			for(int j=0;j<i;j++){
+				if(a[i]==a[j]){
+					flag=false;
+				}
+			}
+		
+		if(flag==true){
+		for(int k=0;k<11;k++){
+			if(a[i]==a[k]){
+				count++;
+			}
+		}
+		}
+		if(count==1){
+	System.out.println(a[i]);	
+	
+		}
+	}
+
+}}
