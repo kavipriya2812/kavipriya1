7d56ef6660f855a95cf3a7f1f79a148b2087cb38
kavipriya2812 <kavipriyakailasam@gmail.com>
2017-08-09 15:21:22 +0530

    Add files via upload

---
+++ jhgvbxhc.java
@@ -0,0 +1,16 @@
+package rrr;
+import java.util.*;
+public class jhgvbxhc {
+
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		String n1=scan.next();
+		String n2=scan.next();
+		int num1=Integer.parseInt(n1);
+        int num2=Integer.parseInt(n2);
+        int mul=num1*num2;
+        String result=Integer.toString(mul);
+        System.out.println(result);
+	}
+
+}
