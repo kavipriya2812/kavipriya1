d7cba7ba39f6c2de80ad7c60a87f8e4b94f3ee38
kavipriya2812 <kavipriyakailasam@gmail.com>
2017-08-11 10:04:09 +0530

    Add files via upload

---
+++ Beginner42.java
@@ -0,0 +1,13 @@
+package guvi;
+
+public class Beginner42 {
+public static void main(String args[]){
+	String str1="kk";
+	String str2="kk";
+	if(str1.equals(str2)){
+		System.out.println("equal");
+	}else{
+		System.out.println("not equal");
+	}
+}
+}
