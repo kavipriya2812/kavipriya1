949bf6d7b201d38b28b1bcfb9a0ca1e5c32502e4
kavipriya2812 <kavipriyakailasam@gmail.com>
2017-08-12 11:41:28 +0530

    Add files via upload

---
+++ Beginner60.java
@@ -0,0 +1,16 @@
+package guvi;
+import java.util.*;
+public class Beginner60 {
+
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+        int n=scan.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++){
+        	a[i]=scan.nextInt();
+        }
+        Arrays.sort(a);
+        System.out.println(a[0]+a[n-1]);
+	}
+
+}
