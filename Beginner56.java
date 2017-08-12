fb4883d608d77f2a3d66636ca246639a274606e8
kavipriya2812 <kavipriyakailasam@gmail.com>
2017-08-12 10:51:50 +0530

    Add files via upload

---
+++ Beginner56.java
@@ -0,0 +1,13 @@
+package guvi;
+import java.util.*;
+public class Beginner56 {
+
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		String name=scan.nextLine();
+		String title=scan.nextLine();
+		System.out.println(title+","+name.charAt(0));
+
+	}
+
+}
