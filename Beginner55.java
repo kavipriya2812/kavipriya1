09f41fd8b57e22cf1d9b960bf55170c1e6a6a50e
kavipriya2812 <kavipriyakailasam@gmail.com>
2017-08-12 10:46:46 +0530

    Add files via upload

---
+++ Beginner55.java
@@ -0,0 +1,16 @@
+package guvi;
+import java.util.*;
+public class Beginner55 {
+
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		String str=scan.nextLine();
+		if(str.charAt(0)>='0'&& str.charAt(0)<='9'){
+			boolean output=true;
+			System.out.println(output);
+		}else{
+			boolean output=false;
+			System.out.println(output);
+		}
+	}
+}
