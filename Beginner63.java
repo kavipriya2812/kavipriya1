a804b2b785297d0a0dddd389251521baa105b2bd
kavipriya2812 <kavipriyakailasam@gmail.com>
2017-08-12 11:55:29 +0530

    Add files via upload

---
+++ Beginner63.java
@@ -0,0 +1,12 @@
+package guvi;
+import java.util.*;
+public class Beginner63 {
+
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+         String str=scan.nextLine();
+         String[] words=str.split(" ");
+         System.out.println(words.length);
+	}
+
+}
