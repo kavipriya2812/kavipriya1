d353567a763ddb42df794ba66e05ae8b3451c5fb
kavipriya2812 <kavipriyakailasam@gmail.com>
2017-08-12 20:17:02 +0530

    Add files via upload

---
+++ Beginner20.java
@@ -0,0 +1,15 @@
+package beginner;
+import java.util.*;
+public class Beginner20 {
+
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		int n=scan.nextInt();
+		for(int i=1;i<=10;i++){
+			int b=i*n;
+			System.out.print(b+" ");
+		}
+
+	}
+
+}
