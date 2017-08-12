2d59a2c497fb0d576f140e34486a003dca3feeb8
kavipriya2812 <kavipriyakailasam@gmail.com>
2017-08-12 13:56:08 +0530

    Add files via upload

---
+++ Hunter57.java
@@ -0,0 +1,15 @@
+package guvi;
+import java.util.*;
+public class Hunter57 {
+
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		int a[]={2,45,67,43,56};
+		int l=a.length;
+		String s[]=new String[l];
+		for(int i=0;i<a.length;i++){
+		 s[i]=Integer.toString(a[i]);
+          if(s[i].length()==1){
+        	  System.out.println(s[i]);
+          }
+	}}}
