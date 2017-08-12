11762d96695a5ec1872a80bce12027deebcf1297
kavipriya2812 <kavipriyakailasam@gmail.com>
2017-08-12 12:08:46 +0530

    Add files via upload

---
+++ Hunter11.java
@@ -0,0 +1,18 @@
+package guvi;
+import java.util.*;
+public class Hunter11 {
+
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		int n=scan.nextInt();
+		String s[]=new String[n];
+		for(int i=0;i<n;i++){
+			s[i]=scan.next();
+		}
+		for(int i=n-1;i>=0;i--){
+			System.out.print(s[i]+" ");
+		}
+
+	}
+
+}
