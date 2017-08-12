5ed58ad9064f87a53e921232d6cc322598e3b554
kavipriya2812 <kavipriyakailasam@gmail.com>
2017-08-12 12:22:42 +0530

    Add files via upload

---
+++ Hunter51.java
@@ -0,0 +1,16 @@
+package guvi;
+import java.util.*;
+public class Hunter51 {
+
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		String str=scan.nextLine();
+		String str1=scan.nextLine();
+int n=Integer.parseInt(str);
+int n1=Integer.parseInt(str1);
+int mul=n*n1;
+String s=Integer.toString(mul);
+System.out.println(s);
+	}
+
+}
