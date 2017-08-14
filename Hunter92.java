4f356cfc193b92d04c5a727f6fa9a4128a290cfd
kavipriya2812 <kavipriyakailasam@gmail.com>
2017-08-14 12:25:43 +0530

    Add files via upload

---
+++ Hunter92.java
@@ -0,0 +1,14 @@
+package guvi;
+import java.util.*;
+public class Hunter92 {
+
+	public static void main(String[] args) {
+	Scanner scan=new Scanner(System.in);
+	String str=scan.nextLine();
+	String h[]=str.split(" ");
+	str = str.trim().replaceAll("\\s+", " ");
+	System.out.println(str);
+
+	}
+
+}
