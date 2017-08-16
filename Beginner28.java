8a18da0664fb65ce4375079c4f1867c49cc594f7
kavipriya2812 <kavipriyakailasam@gmail.com>
2017-08-16 19:30:52 +0530

    Add files via upload

---
+++ Beginner28.java
@@ -0,0 +1,15 @@
+package beginner;
+import java.util.*;
+public class Beginner28 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner scan=new Scanner(System.in);
+System.out.println("enter a character");
+String str=scan.nextLine();
+if(str.charAt(0)=='q'){
+	System.exit(0);
+}
+	}
+
+}
