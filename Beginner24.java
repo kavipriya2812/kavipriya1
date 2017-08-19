5a6b74020533449ee4ae4621ad435b1e19114546
kavipriya2812 <kavipriyakailasam@gmail.com>
2017-08-19 17:10:19 +0530

    Add files via upload

---
+++ Beginner24.java
@@ -0,0 +1,14 @@
+package guvi;
+import java.util.*;
+public class Beginner24 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner scan=new Scanner(System.in);
+int n=scan.nextInt();
+String str=Integer.toString(n);
+String reverse=new StringBuffer(str).reverse().toString();
+System.out.println(reverse);
+	}
+
+}
