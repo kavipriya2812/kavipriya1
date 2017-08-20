38ecd0197bdced4f3a9f979d7332265b07cf1ed8
kavipriya2812 <kavipriyakailasam@gmail.com>
2017-08-20 10:54:16 +0530

    Add files via upload

---
+++ Player210.java
@@ -0,0 +1,26 @@
+package guvi;
+import java.util.*;
+public class Player210 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner scan=new Scanner(System.in);
+int n=scan.nextInt();
+String str=Integer.toString(n);
+int len=str.length();
+int rem=0;int i=0;
+int a[]=new int[len];
+while(n>0){
+	rem=n%10;
+	a[i]=rem;
+	n=n/10;
+	i++;
+	}
+Arrays.sort(a);
+for(int j=len-1;j>=0;j--){
+	System.out.println(a[j]);
+}
+
+	}
+
+}
