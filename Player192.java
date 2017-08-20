ba8dfe86d810dd0bf93cd8010b2af9dd57de2fad
kavipriya2812 <kavipriyakailasam@gmail.com>
2017-08-20 11:40:23 +0530

    Add files via upload

---
+++ Player192.java
@@ -0,0 +1,16 @@
+package guvi;
+import java.util.*;
+
+public class Player192 {
+	public static void main(String args[]){
+		Scanner scan=new Scanner(System.in);
+        int p=scan.nextInt();
+        int n=scan.nextInt();
+        int r=scan.nextInt();
+        int si=(p*n*r)/100;
+        System.out.println(si);
+	
+	
+	}
+
+}
