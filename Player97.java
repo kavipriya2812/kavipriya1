156186c5672fc37b4ca7e092cfba8f768227789b
kavipriya2812 <kavipriyakailasam@gmail.com>
2017-08-21 09:02:20 +0530

    Add files via upload

---
+++ Player97.java
@@ -0,0 +1,15 @@
+package guvi;
+import java.util.*;
+public class Player97 {
+
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		int n=scan.nextInt();
+		int sum=0;
+		for(int i=1;i<n;i+=2){
+			sum+=i;
+		}
+System.out.println(sum);
+	}
+
+}
