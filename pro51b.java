53d10b3c4e3344ee29efa35832bcd6ddff1e33cc
kavipriya2812 <kavipriyakailasam@gmail.com>
2017-08-22 15:37:20 +0530

    Add files via upload

---
+++ pro51b.java
@@ -0,0 +1,14 @@
+package guvi;
+import java.util.*;
+public class pro51b {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner scan=new Scanner(System.in);
+int rows=scan.nextInt();
+int columns=scan.nextInt();
+int total=((rows-1)+(rows*(columns-1)));
+System.out.println(total);
+	}
+
+}
