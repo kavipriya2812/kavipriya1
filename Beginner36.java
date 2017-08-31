27430f2ac8c8a4a5a2379e58f2915af7a12b5380
kavipriya2812 <kavipriyakailasam@gmail.com>
2017-08-31 09:11:07 +0530

    Add files via upload

---
+++ Beginner36.java
@@ -0,0 +1,17 @@
+package beginner;
+import java.util.*;
+public class Beginner36 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner scan=new Scanner(System.in);
+int x=scan.nextInt();
+int y=scan.nextInt();
+int temp=0;
+temp=x;
+x=y;
+y=temp;
+System.out.println(x+" "+y);
+	}
+
+}
