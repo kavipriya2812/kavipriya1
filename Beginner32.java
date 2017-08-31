3de8b01e31a3f44d3cd5533bbe1f186234f8cfec
kavipriya2812 <kavipriyakailasam@gmail.com>
2017-08-31 08:38:34 +0530

    Add files via upload

---
+++ Beginner32.java
@@ -0,0 +1,13 @@
+package beginner;
+import java.util.*;
+public class Beginner32 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner scan=new Scanner(System.in);
+String str=scan.nextLine();
+String a[]=str.split(" ");
+System.out.println(a.length);
+	}
+
+}
