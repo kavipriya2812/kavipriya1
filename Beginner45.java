4003423b43a4fc4cbd9faa8e8cc515ac996690ae
kavipriya2812 <kavipriyakailasam@gmail.com>
2017-08-31 08:25:35 +0530

    Add files via upload

---
+++ Beginner45.java
@@ -0,0 +1,15 @@
+package beginner;
+import java.util.*;
+public class Beginner45 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner scan=new Scanner(System.in);
+System.out.println("enter the marks");
+int n=scan.nextInt();
+if(n<0 || n>100){
+	System.out.println("error:enter a valid input");
+}
+	}
+
+}
